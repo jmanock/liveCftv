ae3d55ae4106bbc0ec1108527fd602d094b9066c
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2019-05-17 09:57:01 -0400

    working on preds page

--- src/Components/Preds.jsx
+++ src/Components/Preds.jsx
@@ -1,30 +1,34 @@
 import React,{Component} from 'react';
 import logo from '../Images/download.png';
+import sharks from '../Images/sharks.png';
+import pirates from '../Images/pirates.png';
+import streets from '../Images/streets.png';
+import columbus from '../Images/columbus.png';
+import cobras from '../Images/cobras.png';
 
 const Games = [
-  {date:'4-13-2019', game:'Columbus Lions @ Orlando Predators', score:'', watch:''},
-  {date:'4-20-2019', game:'Orlando Predators @ New York Streets', score:'', watch:''},
-  {date:'4-27-2019', game:'Orlando Predators @ Massachusetts Pirates', score:'', watch:''},
-  {date:'5-4-2019', game:'Orlando Predators @ Columbus Lions', score:'', watch:''},
-  {date:'5-12-2019', game:'New York Streets @ Orlando Predators', score:'', watch:''},
-  {date:'5-18-2019', game:'Orlando Predators @ Jacksonville Sharks', score:'0 - 0', watch:''},
-  {date:'5-25-2019', game:'Orlando Predators @ Carolina Cobras', score:'0 - 0', watch:''},
-  {date:'5-31-2019', game:'Jacksonville Sharks @ Orlando Predators', score:'0 - 0', watch:''},
-  {date:'6-15-2019', game:'Massachusetts Pirates @ Orlando Predators', score:'0-0', watch:''},
-  {date:'6-30-2019', game:'Carolina Cobras @ Orlando Predators', score:'', watch:''},
-  {date:'7-5-2019', game:'Jacksonville Sharks @ Orlando Predators', score:'', watch:''},
-  {date:'7-13-2019', game:'Orlando Predators @ Carolina Cobras', score:'', watch:''},
-  {date:'7-19-2019', game:'Columbus Lions @ Orlando Predators', score:'', watch:''},
-  {date:'7-27-2019', game:'Orlando Predators @ Jacksonville Sharks', score:'', watch:''}
-];
+  {date:'4-13-2019', hTeam:'Orlando Predators', vTeam:'Columbus Lions', hTeamLogo:logo,vTeamLogo:columbus, watch:'https://www.youtube.com/embed/WTfKxSPCfaE', score:''},
+  {date:'4-20-2019', hTeam:'NY Streets', vTeam:'Orlando Predators', hTeamLogo:streets, vTeamLogo:logo, watch:'https://www.youtube.com/embed/5caDb_QXjJw'},
+  {date:'4-27-2019', hTeam:'Massachusetts Pirates', vTeam:'Orlando Predators', hTeamLogo:pirates, vTeamLogo:logo, watch:'https://www.youtube.com/embed/Opf8-60l358'},
+  {date:'5-4-2019', hTeam:'Columbus Lions', vTeam:'Orlando Predators', hTeamLogo:columbus, vTeamLogo:logo, watch:'https://www.youtube.com/embed/DgIqv-TET9M'},
+  {date:'5-12-2019', hTeam:'Orlando Predators', vTeam:'NY Streets', hTeamLogo:logo, vTeamLogo:streets, watch:''},
+  {date:'5-18-2019', hTeam:'Jacksonville Sharks', vTeam:'Orlando Predators', hTeamLogo:sharks, vTeamLogo:logo, watch:''},
+  {date:'5-25-2019', hTeam:'Carolina Cobras', vTeam:'Orlando Predators', hTeamLogo:cobras, vTeamLogo:logo, watch:''},
+  {date:'5-31-2019', hTeam:'Orlando Predators', vTeam:'JacksonVille Sharks', hTeamLogo:logo, vTeamLogo:sharks, watch:''},
+  {date:'6-15-2019', hTeam:'Orlando Predators', vTeam:'Massachusetts Pirates', hTeamLogo:logo, vTeamLogo:pirates, watch:''},
+  {date:'6-30-2019', hTeam:'Orlando Predators', vTeam:'Carolina Cobras', hTeamLogo:logo, vTeamLogo:cobras, watch:''},
+  {date:'7-5-2019', hTeam:'Orlando Predators', vTeam:'Jacksonville Sharks', hTeamLogo:logo, vTeamLogo:sharks, watch:''},
+  {date:'7-13-2019', hTeam:'Carolina Cobras', vTeam:'Orlando Predators', hTeamLogo:cobras, vTeamLogo:logo, watch:''},
+  {date:'7-19-2019', hTeam:'Orlando Predators', vTeam:'Columbus Lions', hTeamLogo:logo, vTeamLogo:columbus, watch:''},
+  {date:'7-27-2019', hTeam:'Jacksonville Sharks', vTeam:'Orlando Predators', hTeamLogo:sharks, vTeamLogo:logo, watch:''}
+]
 
 class Preds extends Component{
   render(){
     const PredsGames = Games.map((data, index) =>{
       return <tr key={index}>
         <td>{data.date}</td>
-        <td>{data.game}</td>
-        <td>{data.score}</td>
+        <td><img style={{width:50,height:50}} src={data.hTeamLogo} alt='Home Team Logo'></img>{data.hTeam} vs <img style={{width:50, height:50}} src={data.vTeamLogo} alt='Visitor Team Logo'></img>{data.vTeam}</td>
         <td>{data.watch}</td>
       </tr>
     })
@@ -35,7 +39,6 @@ class Preds extends Component{
             <tr>
               <th scope='col'>Date</th>
               <th scope='col'>Game</th>
-              <th scope='col'>Score</th>
               <th scope='col'>Watch</th>
             </tr>
           </thead>
