31f5cfd35178638c0b7eaa32ada5546e22bd5222
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2019-06-03 08:29:46 -0400

    updated videos

--- src/Components/WatchLive.jsx
+++ src/Components/WatchLive.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const WatchLive = () =>{
-const video = 'https://www.youtube.com/embed/jlGzXVVcHz8';
 
+const video='https://firebasestorage.googleapis.com/v0/b/cftv-fd2b9.appspot.com/o/predsjax.mp4?alt=media&token=264fb066-7d0c-499c-9f46-0fcd3bc55acb';
   return(
     <div className='card'>
       <h3 className='card-title text-center'>Watch Live</h3>
